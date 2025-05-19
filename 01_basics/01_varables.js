806d1235b2dc6d1230e30d8326e7d06e9600bea9
hardik <hardik.subechha456@gmail.com>
2025-05-19 22:58:41 +0530

    variable and datatype notes

---
+++ 01_basics/01_varables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accontEmail = "hardik.subechha456@gmail.com"
+var accountPassword = "1234" //dont use var
+accountCity = "jaipur" 
+let accountState
+
+console.table([accountId, accontEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+// code readability high
+
+console.log( 3+3 )
+console.log("hardik")
+
+//number 
+// bigint
+// string => ""
+// boolean
+// null => standAlone value
+// undefined => not defined 
+// symbol
+// object
+
+console.log(typeof "hardik")
+
+// null type => object 
+// undefined type => undefined
